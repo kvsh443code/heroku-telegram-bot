6dd3c1bf8e35b24680f14c98eafdc6f94963c6bb
Kaveesha Ilukkumbure <15032197+kvsh443@users.noreply.github.com>
2017-10-28 23:02:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,13 @@ token = os.environ['token']
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
