46daa445d60f8648c5cf11370c8d999b2d017987
Kaveesha Ilukkumbure <15032197+kvsh443@users.noreply.github.com>
2017-10-29 02:30:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ def user_greet(message):
 	except:
 		l_name="-"
 	title = message.chat.title
-	bot.send_message(message.chat.id, "*"+title+"*` හි සිටි `_"+name+"_` වන තෝ හිටියත් එකයි! නැතත් එකයි!  👋..`",parse_mode='Markdown')
+	bot.send_message(message.chat.id, "*"+title+"*` හි සිටි `_"+f_name+" "+l_name+"_` වන තෝ හිටියත් එකයි! නැතත් එකයි!  👋..`",parse_mode='Markdown')
 	
 		
 @bot.message_handler(func=lambda message: True)
